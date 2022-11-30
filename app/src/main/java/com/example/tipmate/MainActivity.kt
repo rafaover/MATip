6c036c2c8083d8b6bd599dbc087d8e1b518325c8
Rafael Moreira <rafamsilva@gmail.com>
2022-11-30 15:50:49 +1100

    pre merge

--- app/src/main/java/com/example/tipmate/MainActivity.kt
+++ app/src/main/java/com/example/tipmate/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.tipmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var costOfServiceUser = findViewById<TextView>(R.id.cost_of_Service)
+
+
+        // Executes function
+        calculate()
+
+    }
+    // Function to calculate the TIP when value is added and button pushed
+    private fun calculate(costOfService: Float): Unit{
     }
 }
